b3bc7a80650006fb6ea616b3346645bc2a408b02
Guenther Meyer <d.s.e@sordidmusic.com>
2022-05-16 20:08:11 +0200

    added fixture Eurolite PAR56 RGB, fixed color pink

---
+++ services/dmx/fixtures/eurolite_par56_rgb.py
@@ -0,0 +1,61 @@
+from enum import IntEnum
+from services.dmx.dmx import DmxFixture
+
+
+class Channels(IntEnum):
+    DIMMER_RED = 0
+    DIMMER_GREEN = 1
+    DIMMER_BLUE = 2
+    COLOR_MACRO = 3
+    STROBE_SPEED = 4
+    MODE = 5
+
+
+class EurolitePar65Rgb(DmxFixture):
+    def reset(self):
+        self.set_mode(0, False)
+        self.set_color_black(False)
+        self.set_color_macro(0, False)
+        self.set_strobe_speed(0, False)
+        self.submit()
+
+    def set_mode(self, mode: int, submit=True):
+        """
+             0 -  31    Dimmer
+            32-   63    Decreasing Brightness
+            64 -  95    Increasing Brightness
+            96 - 127    Decreasing then increasing Brightness
+           128 - 159    Auto color mix
+           160 - 191    3 color flash
+           192 - 223    7 color flash
+           224 - 255    Sound control
+        """
+        self.set_channel(Channels.MODE, mode, submit)
+
+    def set_color_red(self, submit=True):
+        self.set_color(255, 0, 0, submit)
+
+    def set_color_green(self, submit=True):
+        self.set_color(0, 255, 0, submit)
+
+    def set_color_blue(self, submit=True):
+        self.set_color(0, 0, 255, submit)
+
+    def set_color_white(self, submit=True):
+        self.set_color(255, 255, 255, submit)
+
+    def set_color_black(self, submit=True):
+        self.set_color(0, 0, 0, submit)
+
+    def set_color(self, red, green, blue, submit=True):
+        self.set_channel(Channels.DIMMER_RED, red, False)
+        self.set_channel(Channels.DIMMER_GREEN, green, False)
+        self.set_channel(Channels.DIMMER_BLUE, blue, False)
+        if submit:
+            self.submit()
+
+    def set_color_macro(self, value: int, submit=True):
+        self.set_channel(Channels.COLOR_MACRO, value, submit)
+
+    def set_strobe_speed(self, value: int, submit=True):
+        self.set_channel(Channels.STROBE_SPEED, value, submit)
--- services/dmx/main.py
+++ services/dmx/main.py
@@ -8,6 +8,7 @@ from services.dmx.dmx import DmxControl
 from services.dmx.fixtures.cameo_superfly_xs_5ch import CameoSuperflyXS
 from services.dmx.fixtures.stairville_led_bar_252_rgb import StairvilleLedBar252Rgb
 from services.dmx.fixtures.stairville_afh_600 import StairvilleAFH600
+from services.dmx.fixtures.eurolite_par56_rgb import EurolitePar65Rgb
 
 
 logging.basicConfig(level=logging.INFO)
@@ -33,8 +34,9 @@ class Service(ServiceBase):
         self.config = config
         self.control = DmxControl()
         self.control.add_fixture('bar', StairvilleLedBar252Rgb, 1)
-        self.control.add_fixture('flower', CameoSuperflyXS, 22)
-        self.control.add_fixture('smoke', StairvilleAFH600, 16)
+        self.control.add_fixture('flower', CameoSuperflyXS, 41)
+        self.control.add_fixture('smoke', StairvilleAFH600, 501)
+        self.control.add_fixture('par56', EurolitePar65Rgb, 51)
         self.control.fixtures['flower'].set_rotation(ROTATION)
         super().__init__(self.name, self.topic, self.commands, self.pub_port)
 
@@ -47,30 +49,39 @@ class Service(ServiceBase):
                 match options[0]:
                     case 'red':
                         self.control.fixtures['bar'].set_color_red()
+                        self.control.fixtures['par56'].set_color_red()
                         self.control.fixtures['flower'].set_color_red()
                     case'green':
                         self.control.fixtures['bar'].set_color_green()
+                        self.control.fixtures['par56'].set_color_green()
                         self.control.fixtures['flower'].set_color_green()
                     case 'blue':
                         self.control.fixtures['bar'].set_color_blue()
+                        self.control.fixtures['par56'].set_color_blue()
                         self.control.fixtures['flower'].set_color_blue()
                     case 'cyan':
                         self.control.fixtures['bar'].set_color(0x00, 0xff, 0xff)
+                        self.control.fixtures['par56'].set_color(0x00, 0xff, 0xff)
                         self.control.fixtures['flower'].set_color_black()
                     case 'magenta':
                         self.control.fixtures['bar'].set_color(0xff, 0x00, 0xff)
+                        self.control.fixtures['par56'].set_color(0xff, 0x00, 0xff)
                         self.control.fixtures['flower'].set_color_black()
                     case 'yellow':
                         self.control.fixtures['bar'].set_color(0xff, 0xff, 0x00)
+                        self.control.fixtures['par56'].set_color(0xff, 0xff, 0x00)
                         self.control.fixtures['flower'].set_color_black()
                     case 'pink':
-                        self.control.fixtures['bar'].set_color(0xff, 0xc0, 0xcb)
+                        self.control.fixtures['bar'].set_color(0xff, 0x14, 0x93)
+                        self.control.fixtures['par56'].set_color(0xff, 0x14, 0x93)
                         self.control.fixtures['flower'].set_color_black()
                     case 'white':
                         self.control.fixtures['bar'].set_color_white()
+                        self.control.fixtures['par56'].set_color_white()
                         self.control.fixtures['flower'].set_color_white()
                     case 'black':
                         self.control.fixtures['bar'].set_color_black()
+                        self.control.fixtures['par56'].set_color_black()
                         self.control.fixtures['flower'].set_color_black()
                     case _:
                         log.info(f'unmapped color: {options[0]}')
